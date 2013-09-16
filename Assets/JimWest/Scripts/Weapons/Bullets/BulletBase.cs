b0050f193d7ec10b60e2d4e3b82927e550249b1b
JimWest <j1m_w3st@web.de>
2013-09-16 21:54:53 +0200

    Chagned SimpleBullet to BulletBase

---
+++ Assets/JimWest/Scripts/Weapons/Bullets/BulletBase.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CapsuleCollider))]
+[RequireComponent (typeof (Rigidbody))]
+public class BulletBase : MonoBehaviour {
+
+	public GameObject destroyPrefab;
+	public int damageOnHit = 10;
+	public float speed = 20f;
+	public float forceOnImpact = 20.0f;	
+	
+	private float dist = 50f;
+	private GameObject owner;
+	private Transform tr;
+	private bool valuesSet = false;
+	private string ignoreTag;
+	
+	void OnBecameInvisible (){
+		Destroy (this.gameObject);	
+	}
+		
+	// set the start values for the bullet
+	public virtual void SetStartValues(GameObject owner, Vector3 forward) {
+		this.owner = owner;
+		this.ignoreTag = owner.tag;	
+		this.rigidbody.velocity = forward * speed;
+	}	
+		
+	//void  OnTriggerEnter (Collider collision) {
+	void OnTriggerEnter(Collider enterObj) {
+		if (enterObj.tag != ignoreTag & enterObj.tag != "Bullet") {
+			
+			// add force to the object
+			if (enterObj.rigidbody) {
+				enterObj.rigidbody.AddForceAtPosition (transform.forward, enterObj.transform.position, ForceMode.Impulse);			
+			}
+			
+			// TODO
+			// aply damage
+			HealthHandler health = (HealthHandler)enterObj.GetComponentInChildren<HealthHandler>();			
+			if (health) {
+				health.DeductHealth(damageOnHit);				
+			}
+			
+			if (destroyPrefab) {
+				// show the explosion
+				Vector3 destroyPos = collider.transform.position;
+				destroyPos += transform.forward;
+				GameObject obj = (GameObject)Instantiate(destroyPrefab, destroyPos, Quaternion.identity);
+				//obj.particleSystem.Play();
+				Destroy (obj, 0.5f);
+			}
+			Destroy (this.gameObject);
+			
+		}
+	}
+	
+}
--- Assets/JimWest/Scripts/Weapons/WeaponScript.cs
+++ Assets/JimWest/Scripts/Weapons/WeaponScript.cs
@@ -8,9 +8,6 @@ public class WeaponScript : MonoBehaviour {
 	public GameObject bulletPrefab;
 	public float frequency  = 10f;
 	public float coneAngle = 1.5f;
-	public bool firing = false;
-	public float damagePerSecond = 20.0f;
-	public float forcePerSecond  = 20.0f;
 	
 	private float lastFireTime = -1f;
 	private RigidPlayerScript playerScript;
@@ -32,50 +29,37 @@ public class WeaponScript : MonoBehaviour {
 			if (Input.GetButtonDown("Fire1") | Input.GetButton("Fire1")) {				
 				
 				if (Time.time > lastFireTime + 1 / frequency) {
-					// Spawn visual bullet	
 					
-					//Quaternion tempRot =  Quaternion.LookRotation(playerScript.GetMouseOnPlane() - muzzlePosition.position , Vector3.forward);
-					//tempRot.z = 0f;
-					//tempRot.w = 0.5f;
+					// forward vector
+					Vector3 endPoint = playerScript.GetMouseOnPlane();
+					Vector3 direction = endPoint - muzzlePosition.position;
+					direction.Normalize();	
 					
-					Quaternion tempRot = transform.parent.transform.rotation;
+					// apply scatter
+					Quaternion tempRot = bulletPrefab.transform.rotation;
+					Quaternion test = Quaternion.FromToRotation(bulletPrefab.transform.position, direction);
+					Debug.Log (test.eulerAngles.y);
 					
-					Quaternion coneRandomRotation = Quaternion.Euler (Random.Range (-coneAngle, coneAngle), Random.Range (-coneAngle, coneAngle), 0);
-					GameObject go = (GameObject)Instantiate (bulletPrefab, muzzlePosition.position, tempRot * coneRandomRotation);
-					SimpleBullet bullet = go.GetComponent<SimpleBullet> ();
+					//tempRot.y = Quaternion.FromToRotation(transform.position, endPoint).y;
+					//Quaternion coneRandomRotation = Quaternion.Euler (Random.Range (-coneAngle, coneAngle), Random.Range (-coneAngle, coneAngle), 0);
+					//tempRot *= coneRandomRotation;
 					
-					muzzleParticle.Emit(1);
-									
+					//Debug.Log (tempRot.ToString ());
+					//tempRot.y = transform.rotation.y;
+
+					// Spawn visual bullet	and set values for start					
+					GameObject go = (GameObject)Instantiate (bulletPrefab, muzzlePosition.position, bulletPrefab.transform.rotation);
+					BulletBase bullet = go.GetComponent<BulletBase> ();
+					go.transform.RotateAround(bullet.transform.position, Vector3.up, test.eulerAngles.y);
+					bullet.SetStartValues(playerScript.gameObject, direction);
+					
+					// show visul muzzle
+					muzzleParticle.Emit(1);									
 					lastFireTime = Time.time;
-					
-					// Find the object hit by the raycast
-					RaycastHit hitInfo = new RaycastHit ();
-					Physics.Raycast (transform.position, transform.parent.forward, out hitInfo, 100);
-	
-					if (hitInfo.transform) {
-						// Get the health component of the target if any
-						HealthHandler targetHealth = hitInfo.transform.GetComponent<HealthHandler> ();
-						
-						if (targetHealth & (hitInfo.transform.root != transform.root)) {
-							// Apply damage
-							targetHealth.DeductHealth(Mathf.FloorToInt(damagePerSecond / frequency));
-						}
-						
-						// Get the rigidbody if any
-						if (hitInfo.rigidbody) {
-							// Apply force to the target object at the position of the hit point
-							Vector3 force = transform.forward * (forcePerSecond / frequency);
-							hitInfo.rigidbody.AddForceAtPosition (force, hitInfo.point, ForceMode.Impulse);
-						}					
-						bullet.dist = hitInfo.distance;
-					}
-					else {
-						bullet.dist = 1000;
-					}
 				}				
 			}
 			
 		}
-	}
+	}	
 
 }
