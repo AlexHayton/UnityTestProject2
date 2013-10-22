3df65dbbdf891808d535c2c14580cd797ed2876f
Alex Hayton <alex.hayton@gmail.com>
2013-10-22 08:07:53 +0100

    Added a real XP Bar

---
+++ Assets/TestProject/Scripts/GUI/HUD/GUIXPBar.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+using System;
+	
+[ExecuteInEditMode()]
+public class GUIXPBar : GUIHorizontalBar {
+
+	private GameObject player;
+
+	public override void Start()
+	{
+		base.Start();
+		player = PlayerUtility.GetPlayer ();
+	}
+	
+	public override float GetFullScalar()
+	{
+		if (Application.isPlaying)
+		{
+			XPHandler handler = player.GetComponent<XPHandler>();
+			return handler.GetXpTillNextLevelScalar();
+		}
+		else
+		{
+			return 1.0f;
+		}
+	}	
+	
+	public override string GetText()
+	{
+		XPHandler handler = player.GetComponent<XPHandler>();
+		float nextLevel = handler.GetXpForNextLevel();
+		float currentXp = handler.GetXp();
+		
+		nextLevel = Mathf.Ceil (nextLevel);
+		currentXp = Mathf.Ceil (currentXp);
+		return ( currentXp + "/" + nextLevel );
+	}
+}
+
--- Assets/TestProject/Scripts/Handlers/XPHandler.cs
+++ Assets/TestProject/Scripts/Handlers/XPHandler.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using System.Collections;
+using System;
 
 public class XPHandler : MonoBehaviour {
 	
 	public float xp = 0;
+	public float xpPerLevel = 1000;
+	public float xpScalarPerLevel = 1.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +20,40 @@ public class XPHandler : MonoBehaviour {
 	public void AddXp(float xpToAdd)
 	{
 		this.xp += xpToAdd;
+	}
+	
+	public float GetXpForLevel(int level)
+	{
+		return (float) (xpPerLevel * xpScalarPerLevel * level);
+	}
+	
+	public int GetNextLevel()
+	{
+		return this.GetLevel() + 1;
+	}
+	
+	public int GetLevel()
+	{
+		float lvlFloat = (float)(this.GetXp() / (xpPerLevel * xpScalarPerLevel));
+		return (int)Math.Floor(lvlFloat);
+	}
+	
+	public float GetXpForThisLevel()
+	{
+		return this.GetXpForLevel(this.GetLevel());
+	}
+	
+	public float GetXpForNextLevel()
+	{
+		return this.GetXpForLevel(this.GetNextLevel());
+	}
+	
+	public float GetXpTillNextLevelScalar()
+	{
+		float thisLevel = this.GetXpForThisLevel();
+		float nextLevel = this.GetXpForNextLevel();
+		float currentXp = this.GetXp();
+		return (currentXp - thisLevel) / nextLevel;
 	}
 
 }
