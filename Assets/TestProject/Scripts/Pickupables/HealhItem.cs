ff74c23e3384a24727c27d31a451b0f0dec740f7
Jibrail <Rami.Majid@hotmail.com>
2013-11-07 18:25:58 +0100

    added hp and energy pickups, and tied the HP to current existing script and changed value of each pick to 10hp added multiple hp pickups to ship scene fixed physics on some of the stuff on there, the chair dosn't rotate in air once its shot anymore. read my posts on campfire for more.

--- Assets/TestProject/Scripts/Pickupables/HealhItem.cs
+++ Assets/TestProject/Scripts/Pickupables/HealhItem.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class HealhItem : Pickupable {
 		
-	public int addedHealth = 20;
+	public int addedHealth = 10;
 
 
 	public override bool OnPickUp (GameObject player)
