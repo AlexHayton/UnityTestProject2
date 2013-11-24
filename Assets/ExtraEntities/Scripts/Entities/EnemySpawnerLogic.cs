e73776ae3cd7e3e6c0cffa61f51f1e8f2495b3f4
Alex Hayton <alex.hayton@gmail.com>
2013-11-24 16:57:17 +0000

    Improve enemy spawners

--- Assets/ExtraEntities/Scripts/Entities/EnemySpawnerLogic.cs
+++ Assets/ExtraEntities/Scripts/Entities/EnemySpawnerLogic.cs
@@ -6,25 +6,30 @@ public class EnemySpawnerLogic : Entity {
 	public GameObject enemyPrefab;
 	public int spawnAmount = 4;
 	public float spawnTime = 4f;
+	public int numberOfWaves = 1;
+	private int spawnedWaves = 0;
+	public bool infiniteWaves = true;
 	
 	private float lastSpawn = 0f;
 
 	// Use this for initialization
 	void Start () {
-	
+		InvokeRepeating("CheckWhetherToSpawn", 0, spawnTime);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (isEnabled){
-			if (lastSpawn == 0 | Time.time > lastSpawn + spawnTime) {
-				if (enemyPrefab) {
-					for (int i = 0; i < spawnAmount; i++) {
-						Instantiate(enemyPrefab, transform.position, transform.rotation);	
-					}
+	void CheckWhetherToSpawn () 
+	{
+		if (isEnabled && (infiniteWaves || numberOfWaves < spawnedWaves))
+		{
+			if (enemyPrefab) 
+			{
+				for (int i = 0; i < spawnAmount; i++) 
+				{
+					Instantiate(enemyPrefab, transform.position, transform.rotation);	
 				}
-				lastSpawn = Time.time;
 			}
+			spawnedWaves++;
 		}
 	}
 }
