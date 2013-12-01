2a3ae7309e1faed2d3f4ab85690b5722fa3a07a1
Alex Hayton <alex.hayton@gmail.com>
2013-12-01 10:11:10 +0000

    Got a basic idle and walk animation in there. Put the gun on the guy's head...

--- Assets/TestProject/Scripts/PlayerControll/RigidPlayerScript.cs
+++ Assets/TestProject/Scripts/PlayerControll/RigidPlayerScript.cs
@@ -21,6 +21,7 @@ public class RigidPlayerScript : MonoBehaviour
 	public GameObject onHitGui;
 
     private Transform gripPoint;
+	private Animator animator;
 
     [HideInInspector]
     public Transform LaserTransform;
@@ -28,6 +29,8 @@ public class RigidPlayerScript : MonoBehaviour
     private bool grounded = false;
     private Vector3 lookTarget;
     private Vector3 cameraOffset = Vector3.zero;
+
+	private const string ANIM_PARAMS_SPEED = "Speed";
 
     void Awake()
     {
@@ -45,67 +48,84 @@ public class RigidPlayerScript : MonoBehaviour
         mainCamera.transform.position = transform.position - mainCamera.transform.forward * cameraHeight * 10;
         mainCamera.orthographicSize = 13;
         cameraOffset = mainCamera.transform.position - transform.position;
+
+		animator = this.GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.F1))
-        {
-            Application.LoadLevel(Application.loadedLevel);
-        }
-
-		if (Input.GetKeyDown (KeyCode.Escape)) 
-		{
-			Application.LoadLevel (1);
-		}
-
-        if (grounded)
-        {
-
-            //************************************
-            // Rotation
-            //************************************
-
-            // rotate towards target
-            //Vector3 muzzleToMouse = (GetMouseOnPlane(new Plane(Vector3.up, GetComponentInChildren<WeaponScript>().PrimaryWeapon.muzzlePosition.transform.position)) - GetComponentInChildren<WeaponScript>().PrimaryWeapon.muzzlePosition.transform.position);
-            //muzzleToMouse.y = 0;
-            //Vector3 playerToMuzzle = GetComponentInChildren<WeaponScript>().PrimaryWeapon.muzzlePosition.transform.position - transform.position;
-            //playerToMuzzle.y = 0;
-
-            //************************************
-            // Movement
-            //************************************
-
-            // Calculate how fast we should be moving
-            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-            //rotate for isometric
-            targetVelocity = new Vector3(targetVelocity.x + targetVelocity.z, 0, targetVelocity.z - targetVelocity.x) * Mathf.Sqrt(2) / 2;
-
-            if (targetVelocity.sqrMagnitude > 1f)
-                targetVelocity.Normalize();
-            //targetVelocity = transform.TransformDirection(targetVelocity);
-            targetVelocity *= speed;
-
-            // Apply a force that attempts to reach our target velocity
-            Vector3 velocityChange = (targetVelocity - rigidbody.velocity);
-            //velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-            //velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-            //velocityChange.y = 0;
-            rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
-
-        }
-
-        grounded = false;
+		UpdateVelocity();
     }
 
 	void Update()
 	{
-		if (gripPoint == null)
-		{
-			Debug.LogError("Grip Point is null!");
-		}
+		HandleSpecialInput();
+		UpdateRotation();
+		UpdateAnimationParameters();
+	}
 
+	void LateUpdate()
+	{
+		UpdateCamera();	
+	}
+	
+	void HandleSpecialInput()
+	{
+		if (Input.GetKeyDown(KeyCode.F1))
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+		
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			Application.LoadLevel (1);
+		}
+	}
+
+	void UpdateVelocity()
+	{
+		if (grounded)
+		{
+			
+			//************************************
+			// Rotation
+			//************************************
+			
+			// rotate towards target
+			//Vector3 muzzleToMouse = (GetMouseOnPlane(new Plane(Vector3.up, GetComponentInChildren<WeaponScript>().PrimaryWeapon.muzzlePosition.transform.position)) - GetComponentInChildren<WeaponScript>().PrimaryWeapon.muzzlePosition.transform.position);
+			//muzzleToMouse.y = 0;
+			//Vector3 playerToMuzzle = GetComponentInChildren<WeaponScript>().PrimaryWeapon.muzzlePosition.transform.position - transform.position;
+			//playerToMuzzle.y = 0;
+			
+			//************************************
+			// Movement
+			//************************************
+			
+			// Calculate how fast we should be moving
+			Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			
+			//rotate for isometric
+			targetVelocity = new Vector3(targetVelocity.x + targetVelocity.z, 0, targetVelocity.z - targetVelocity.x) * Mathf.Sqrt(2) / 2;
+			
+			if (targetVelocity.sqrMagnitude > 1f)
+				targetVelocity.Normalize();
+			//targetVelocity = transform.TransformDirection(targetVelocity);
+			targetVelocity *= speed;
+			
+			// Apply a force that attempts to reach our target velocity
+			Vector3 velocityChange = (targetVelocity - rigidbody.velocity);
+			//velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+			//velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+			//velocityChange.y = 0;
+			rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+			
+		}
+		
+		grounded = false;
+	}
+	
+	void UpdateRotation()
+	{
 		if(LaserTransform != null)
 		{
 			var lookDir = GetMouseOnPlane(new Plane(Vector3.up, gripPoint.position)) - transform.position;
@@ -116,20 +136,21 @@ public class RigidPlayerScript : MonoBehaviour
 			targetRot.z = 0;
 			
 			float rotSpeed = turnSpeed * Time.deltaTime;
-			transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, 1000);
-		}
-		else
-		{
-			Debug.Log ("Player isn't grounded");
+			rigidbody.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRot, 1000));
 		}
 	}
 
-	void LateUpdate()
+	void UpdateAnimationParameters()
+	{
+		this.animator.SetFloat(ANIM_PARAMS_SPEED, this.rigidbody.velocity.magnitude);
+	}
+
+	void UpdateCamera()
 	{
 		//MoveCamera
-		var enemyDir = GetDirectionOfenemies();
-		var cameraDestination = transform.position + cameraOffset + enemyDir.normalized;
-		var cameraDestinationSize = 8 + enemyDir.magnitude * .2f;
+		Vector3 enemyDir = GetDirectionOfenemies();
+		Vector3 cameraDestination = transform.position + cameraOffset + enemyDir.normalized;
+		float cameraDestinationSize = 8 + enemyDir.magnitude * .2f;
 		mainCamera.orthographicSize += (cameraDestinationSize - mainCamera.orthographicSize) * Time.fixedDeltaTime;
 		mainCamera.transform.position += (cameraDestination - mainCamera.transform.position) * Time.fixedDeltaTime;
 	}
@@ -196,5 +217,4 @@ public class RigidPlayerScript : MonoBehaviour
 	public Camera GetPlayerCamera() {
 		return this.mainCamera;
 	}
-
 }
