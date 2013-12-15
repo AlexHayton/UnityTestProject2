775adc2265cb677d37683c61553f8b4652420d9f
JimWest <j1m_w3st@web.de>
2013-12-15 12:19:05 +0100

    Nearly finished new rangedWeapon script

--- Assets/TestProject/Scripts/Weapons/RangedWeapon.cs
+++ Assets/TestProject/Scripts/Weapons/RangedWeapon.cs
@@ -1,20 +1,108 @@
-using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TestProject;
+using Debug = System.Diagnostics.Debug;
+using Random = UnityEngine.Random;
  
 public class RangedWeapon : Weapon  {
+	public float coneAngle = 1.5f;
+	public int bulletsToCreate = 1;
+	public float bulletSpeed = 20.0f;
 	public GameObject muzzleEffect;
-	public float muzzleEffectTime;
-	public GameObject bulletPrefab;
-	public float coneAngle;
-	public int bulletsToCreate;
-	public float bulletSpeed;
-	protected String muzzleEffectObject;
+	public float muzzleEffectTime = 0.1f;
+	public GameObject projectilePrefab;
+	public Transform bulletOrigin;
+
+	protected MuzzleFlashBase muzzleFlash;
+
+	// Lazy-Cache the bullet start values
+	protected bool initialisedStartValues = false;
+	protected BulletBase.StartValues m_BulletStartValues;
+	protected BulletBase.StartValues BulletStartValues
+	{
+		get
+		{
+			if (!initialisedStartValues)
+			{
+				m_BulletStartValues = new BulletBase.StartValues()
+				{
+					owner = this.owner,
+					DamageOnHit = this.damageOnHit,
+					ForceOnImpact = this.forceOnImpact
+				};
+				initialisedStartValues = true;
+			}
+			return m_BulletStartValues;
+		}
+	}
+
+	public override void Start() {
+		base.Start ();
+
+		GameObject muzzleFlashObject = Instantiate(muzzleEffect) as GameObject;
+		muzzleFlashObject.transform.parent = bulletOrigin;
+		muzzleFlashObject.transform.localRotation = muzzleEffect.transform.rotation;
+		muzzleFlashObject.transform.localPosition = muzzleEffect.transform.position;
+		muzzleFlash = muzzleFlashObject.GetComponent<MuzzleFlashBase>();
+	}
+
+	public override bool Attack() {
+		if (base.Attack ()) {
+
+			// Spawn visual bullet	and set values for start				
+			for (int i = 0; i < bulletsToCreate; i++)
+			{
+				this.SpawnProjectile();
+			}
+			
+			// show visul muzzle
+			if (muzzleFlash != null)
+			{
+				ParticleSystem particleSys = muzzleFlash.GetComponent<ParticleSystem>();
+				if (particleSys != null)
+				{
+					particleSys.Emit(1);
+				}
+				
+				if (muzzleFlash != null)
+				{
+					muzzleFlash.Fire(this.muzzleEffectTime);
+				}
+			}
+
+			return true;
+		}
+		return false;
+
+	}
 
 	public void SpawnProjectile() {
-		throw new System.Exception("Not implemented");
+		// forward vector
+		Vector3 direction = transform.forward.normalized;
+		
+		// apply scatter
+		Vector3 dirWithConeRandomization = direction + new Vector3(Random.Range(-coneAngle, coneAngle), 0, Random.Range(-coneAngle, coneAngle));
+		float spreadAngle = Vector3.Angle(direction, dirWithConeRandomization);
+		Quaternion tempRot = projectilePrefab.transform.rotation;
+		tempRot.SetFromToRotation(projectilePrefab.transform.forward, dirWithConeRandomization);
+		
+		// Set up the new bullet as efficiently as possible
+		GameObject go = Instantiate(projectilePrefab, bulletOrigin.position, tempRot) as GameObject;
+		BulletBase bullet = go.GetComponent<BulletBase>();
+		float actualbulletSpeed = this.bulletSpeed * Mathf.Cos(Mathf.Deg2Rad * spreadAngle);
+		// Use cached bullet startValues when possible
+		BulletBase.StartValues values = this.BulletStartValues;
+		values.forward = dirWithConeRandomization;
+		values.Speed = actualbulletSpeed;
+		bullet.SetStartValues(values);
 	}
+
 	protected void SetProjectileValues() {
 		throw new System.Exception("Not implemented");
 	}
+
+	
 
 }
--- Assets/TestProject/Scripts/Weapons/Weapon.cs
+++ Assets/TestProject/Scripts/Weapons/Weapon.cs
@@ -1,42 +1,79 @@
 using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TestProject;
+using Debug = System.Diagnostics.Debug;
+using Random = UnityEngine.Random;
 
-public abstract class Weapon : MonoBehaviour  {
-	public float range;
-	public int damageOnHit;
-	public float forceOnImpact;
-	public float coolDown;
+public class Weapon : MonoBehaviour  {
+	public float range = 200f;
+	public int damageOnHit = 10;
+	public float forceOnImpact = 20f;
+	public float coolDown = .1f;
+	public int weight = 10;
 	public Transform gripPoint;
-	public int weight;
 	public Texture2D icon;
-	public AudioClip[] sounds;
+	public List<AudioClip> sounds;
+	public GameObject pickupPrefab;
+
 	protected float lastAttack;
 	protected GameObject owner;
 	protected AudioSource audioObject;
 
-	public GameObject getOwner() {
+	public virtual void Start() {
+		this.owner = this.transform.parent.gameObject;
+	}
+
+	public virtual void Update() {
+	}
+
+	public virtual bool Attack() {
+		if (Time.time - lastAttack > coolDown) {
+			this.PlayAttackSound();
+			lastAttack = Time.time;
+			return true;
+		}
+		return false;
+	}
+
+	public virtual void AltAttack() {
+		throw new System.Exception("Not implemented");
+	}
+
+	protected virtual void PlayAttackSound() {
+		if (sounds.Any())
+		{
+			AudioSource.PlayClipAtPoint(sounds[Random.Range(0, sounds.Count - 1)], transform.position);
+		}
+	}
+
+	public Texture2D getIcon()
+	{
+		return this.icon;
+	}
+		
+	public virtual GameObject getOwner() {
 		return this.owner;
 	}
-	public void setOwner(GameObject aOwner) {
+
+	public virtual void setOwner(GameObject aOwner) {
 		this.owner = aOwner;
 	}
-	public int getDamage() {
-		throw new System.Exception("Not implemented");
+
+	public virtual GameObject getPickupPrefab()
+	{
+		return this.pickupPrefab;
 	}
-	public void Start() {
-		throw new System.Exception("Not implemented");
-	}
-	public void Update() {
-		throw new System.Exception("Not implemented");
-	}
-	public bool Attack() {
-		throw new System.Exception("Not implemented");
-	}
-	public void AltAttack() {
-		throw new System.Exception("Not implemented");
-	}
-	protected void PlayAttackSound() {
-		throw new System.Exception("Not implemented");
+
+	public virtual void Drop()
+	{
+		if (this.pickupPrefab)
+		{
+			Instantiate(this.pickupPrefab, transform.position, transform.rotation);
+			Destroy (this);
+		}
 	}
 
 
