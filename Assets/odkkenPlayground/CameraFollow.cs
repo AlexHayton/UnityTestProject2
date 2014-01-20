db1c11f5d1bda4c9b83d9ba4677942b7d8cbce42
odkken <odkken@gmail.com>
2014-01-19 19:07:07 -0800

    gg

---
+++ Assets/odkkenPlayground/CameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Vector3 CameraAngle;
+    public float CameraDistance = 12;
+    public float CameraHeightOffset = 0;
+
+    public bool MouseLead = false;
+    public float LeadAmount = .1f;
+
+    [HideInInspector]
+    public Transform MainCameraTransform;
+
+    // Use this for initialization
+    void Awake()
+    {
+        MainCameraTransform = transform.FindChild("PlayerCamera");
+        MainCameraTransform.parent = null;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //start out with normal orientation and position
+        MainCameraTransform.rotation = Quaternion.Euler(CameraAngle);
+        MainCameraTransform.position = transform.position - MainCameraTransform.forward * CameraDistance;
+
+        //add lead
+        if (MouseLead)
+        {
+            var mouseDirection = GetComponent<MouseLooker>().LookDirection;
+            MainCameraTransform.position += mouseDirection * LeadAmount;
+        }
+
+    }
+}
